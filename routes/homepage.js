69fa585473bf78d9c0257aef1e1a016a8c1b8335
OlaShabalina <o.shabalina@outlook.com>
2021-09-26 19:43:09 +1000

    created a template for details.ejs page to display each movie

--- routes/homepage.js
+++ routes/homepage.js
@@ -4,5 +4,13 @@ const router = express.Router();
 router.get('/', (req,res) => {
     const userId = req.session.userId;
     res.render('./pages/home', { userId });
-}) 
+});
+
+router.get('/:id', (req,res) => {
+    const { id } = req.params;
+    const userId = req.session.userId;
+    res.render('./pages/details', { userId, id });
+});
+
+
 module.exports = router;
--- sql/create_table.sql
+++ sql/create_table.sql
@@ -2,17 +2,18 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS movies;
 
 CREATE TABLE IF NOT EXISTS users (
-    users_id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL, 
     email VARCHAR(255) NOT NULL, 
     password VARCHAR(255) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS movies(
-    movie_id SERIAL PRIMARY KEY,
-    users_id INT NOT NULL,
-    rating INT CHECK (rating BETWEEN 1 AND 5) NOT NULL,
+    review_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    movie_rating INT CHECK (movie_rating BETWEEN 1 AND 5) NOT NULL,
     CONSTRAINT fk_users 
-    FOREIGN KEY(users_id)
-     REFERENCES users(users_id)
-     ON DELETE CASCADE
+        FOREIGN KEY(user_id)
+            REFERENCES users(user_id)
+                ON DELETE CASCADE
 );
