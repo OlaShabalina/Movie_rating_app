7e3a70a87e6a25bab0eeabdec39be3cbe3e269a3
OlaShabalina <o.shabalina@outlook.com>
2021-09-29 22:51:29 +1000

    introduced ajax request to take data from backend (api route). Destructured further and added an additional function showRowWithData

--- public/home.js
+++ public/home.js
@@ -74,24 +74,60 @@ $.ajax(`${base_URL}/genre/movie/list${api_key}`)
 
 })
 
-// Function to generate a list of 20 movies basedd on search criterias
-const getAPIData = function (ajaxRequest) { 
+const averageRating = 3;
+const usersVoted = 20;
+
+// Function to generate movie DOM using ajax request
+const getDataFromAPI = (ajaxRequest) => {
     $.ajax(ajaxRequest)
-            .then(data => {
+    .then(data => {
+        
+        // empty the table before adding movie results
+        $(".film-list").empty()
+        
+        const results = data.results;
 
-            // empty the div before displaying search results
-            $(".film-list").empty();
+        // generating movie tables
+        results.forEach((film, index) => {
+            
+            // if user exists in the system, we generate DOM this way (with user rating column)
+            if (user !== 0) {
+                // getting data from backend on the user ratings
+                $.ajax('/api').then(logedInUserReviews => {
+                    logedInUserReviews.userRatings.forEach(userRating => {
 
-            // adding a line into the table for each movie
-            for (let film of data.results) {
-                let newFilm = $(`<tr>
-                    <th scope="col"> <img src='${poster_URL}${film.poster_path}' style="width: 50px">  </th>
-                    <td>${film.title}</td>
-                    <td><i class="material-icons">star</i> <strong>9.2</strong></td>
-                    <td><i class="material-icons">star_outline</i></td>
-                    <td><a href="${film.id}"><i class="material-icons">info</i></a></td>
-                </tr>`);
-                $(".film-list").append(newFilm)
-            }
-        });
- }
+                        // displaying user rating for the movies which have rating in our database
+                        if (film.id === userRating.movie_id) {
+                            showRowWithData(film, averageRating, usersVoted, userRating.rating);
+                        } 
+                    });
+                    
+                    // now we load the rest of the movies (so the ones with rating don't double up)
+                    if ($(`.movie-title:eq(${index})`).text() !== film.title) {
+
+                        // movies without user rating will have "?" as a text
+                        showRowWithData(film, averageRating, usersVoted, '?');
+                    }
+                })
+                .catch(err => console.log(err));
+                
+            } else {
+                // for users which are not logged in we have a different display (no need to show user rating)
+                showRowWithData(film, averageRating, usersVoted, 'NA');
+            }    
+                
+        })
+    });
+}
+
+// function to generate each row
+const showRowWithData = (film, averageRating, usersVoted, yourVote) => {
+    let newFilm = $(`<tr>
+        <th scope="col"> <img src='${poster_URL}${film.poster_path}' style="width: 50px">  </th>
+        <td class="movie-title">${film.title}</td>
+        <td><i class="material-icons">star</i> <strong>${averageRating}</strong>(${usersVoted})</td>
+        <td><i class="material-icons">star</i>${yourVote}</td>
+        <td><a href="${film.id}"><i class="material-icons">info</i></a></td>
+        </tr>`)
+    $(".film-list").append(newFilm);
+}
